28b48a11f2383ebaaec39b4f81de716342103302
Mohamed El-Refai <elrefai99@gmail.com>
2026-02-18 14:52:45 +0200

    feat: add LoginService and LoginPipe with initial setup, tests, and module integration.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,8 +4,10 @@ import { RegisterController } from './controller/register/register.controller';
 import { ForgetResetPasswordController } from './controller/forget_reset_password/forget_reset_password.controller';
 import { RefreshController } from './controller/refresh/refresh.controller';
 import { AuthController } from './auth.controller';
+import { LoginService } from './controller/login/login.service';
 
 @Module({
-  controllers: [LoginController, RegisterController, ForgetResetPasswordController, RefreshController, AuthController]
+  controllers: [LoginController, RegisterController, ForgetResetPasswordController, RefreshController, AuthController],
+  providers: [LoginService]
 })
 export class AuthModule { }
